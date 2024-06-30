4ebe819f968a9f9a2bcdd4cfa24b8267a2e335f2
Josh Daniels <jdaans95@gmail.com>
2024-06-30 13:43:04 -0700

    declared and initilized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// declare and initilize variables
+
+string name = "Josh";
+int age = 29;
+char middleInitial = 'A';
+bool isOver18 = true;
+double currentTemp = 80;
+decimal currentPrice = 50;
+
+//writing variables to console
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial:{middleInitial}");
+Console.WriteLine($"Is Over 18:{isOver18}");
+Console.WriteLine($"Current Temp:{currentTemp}");
+Console.WriteLine($"Current Price:{currentPrice}");
